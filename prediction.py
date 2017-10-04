c247521a23cf65591eabc313a4ae30331328148b
Deepak345 <senapati.dipak97@gmail.com>
2017-10-04 21:03:50 +0530

    data cleaning

--- prediction.py
+++ prediction.py
@@ -42,3 +42,88 @@ grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
 grid.add_legend()
 plt.show()'''
 
+train = train.drop(['Ticket', 'Cabin'], axis=1)
+test = test.drop(['Ticket', 'Cabin'], axis=1)
+combine = [train, test]
+for dataset in combine:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map({"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5})
+    dataset['Title'] = dataset['Title'].fillna(0)
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+print(train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+train = train.drop(["Name", "PassengerId"], axis=1)
+print(train.head(11))
+
+guess_age = np.zeros((2, 3))
+for i in range(0, 2):
+    for j in range(0, 3):
+        guess = dataset[(dataset['Sex'] == i) & (dataset['Pclass'] == j+1)]['Age'].dropna()
+        age_guess = guess.median()
+        guess_age[i, j] = age_guess            
+for i in range(0, 2):
+    for j in range(0, 3):
+        train.loc[ (train.Age.isnull()) & (train.Sex == i) & (train.Pclass == j+1),'Age'] = guess_age[i,j]
+        test.loc[ (test.Age.isnull()) & (test.Sex == i) & (test.Pclass == j+1),'Age'] = guess_age[i,j]
+train['Age'] = train['Age'].astype(int)
+test['Age'] = test['Age'].astype(int)
+
+train['AgeBand'] = pd.cut(train['Age'], 5)
+test['AgeBand'] = pd.cut(test['Age'], 5)
+print(train[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True))
+combine = [train, test]
+for dataset in combine:    
+    dataset.loc[ dataset['Age'] <= 16, ['Age']] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), ['Age']] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), ['Age']] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), ['Age']] = 3
+    dataset.loc[ dataset['Age'] > 64, ['Age']] = 4
+train = train.drop(['AgeBand'], axis=1)
+test = test.drop(['AgeBand'], axis=1)
+combine = [train, test]
+print(train.head())
+for dataset in combine:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+print(train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+for dataset in combine:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+print(train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+train = train.drop(['Parch', 'SibSp'], axis=1)
+test = test.drop(['Parch', 'SibSp'], axis=1)
+combine = [train, test]
+print(train.head())
+print(test.head())
+for dataset in combine:
+    dataset['Age*Class'] = dataset.Age * dataset.Pclass
+print(train.loc[:, ['Age*Class', 'Age', 'Pclass']].head(10))
+freq_port = train.Embarked.dropna().mode()[0]
+print(freq_port)
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+print(train[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+test['Fare'].fillna(test['Fare'].dropna().median(), inplace=True)
+train['FareBand'] = pd.cut(train['Fare'], 4)
+test['FareBand'] = pd.cut(test['Fare'], 4)
+print(train[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True))
+combine = [test, train]
+for dataset in combine:
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+train = train.drop(['FareBand'], axis=1)
+test = test.drop(['FareBand'], axis=1)
+combine = [train, test]    
+print(train.head(10))
+print(test.head(10))
+test = test.drop(['Name'], axis = 1)
+test.info() 
